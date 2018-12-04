cabec48f4b3cc8a59418819224f229d09e011b7e
talpioter <ityrant@qq.com>
2018-12-04 18:02:30 +0800

    update

--- sever2.py
+++ sever2.py
@@ -31,80 +31,19 @@ class UserHandler(tornado.web.RequestHandler):
         if self.request.files:
             file_name = "%s" % uuid.uuid1()
             print(file_name)
-
-            # file_raw = self.request.files["file"][0]["body"]
-            # iamges = "m_%s.jpg" % file_name
-            #
-            # fin = open(iamges, "w")
-            # print("success to open file")
-            # fin.write(str(file_raw))
-            # fin.close()
-
-            # face_rec_reslut = recognition.face_reconition.images_to_vectors(file_name)
-
             self.write("OK")
-
-            # params = json.loads(self.request.body.decode('utf-8'))
             params = self.request.files['file'][0]['body']
-            print(type(params))
-            print("图片提取成功！")
-            # image_base64 = params["image_base64"]
-            imgfile = base64.b64decode(params)
-            print(type(imgfile))
-
             strtime = time.strftime("%Y%m%d_%H%M%S", time.localtime())
             imgfilename = os.path.join(imgtempdir, strtime + str(random.randint(10000, 99999)) + '.jpg')
-            with open(imgfilename, 'w') as obj:
-                obj.write(imgfile)
-            # logging.info('imgfilename : ' + imgfilename)
-            # try:
-            #     with open(imgfilename, 'w') as obj:
-            #         obj.write(imgfile)
-            # except:
-            #     return {'message': 'write temp img file error'}
-            #
-            # img = cv2.imread(imgfile, 0)
-            # cv2.imshow('image', img)
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
+            with open(imgfilename, 'wb') as obj:
+                obj.write(params)
 
-            # if 'image_base64' in params:
-            #     image_base64 = params["image_base64"]
-            # else:
-            #     return {'message': 'no image_base64'}
-            # # 从base64编码得到图像文件
-            # try:
-            #     imgfile = base64.b64decode(image_base64)
-            # except:
-            #     return{'message': 'base64.b64decode error'}
+            result = recognition.face_reconition.images_to_vectors(self, imgfilename)
 
-            #face_con_result = recognition.face_reconition.images_to_vectors(imgfile)
+            print(result)
 
-            self.render("user.html")
+            self.render("user.html", aa1 =result )
             self.set_header('Content-Type', 'application/json; charset=UTF-8')
-            #
-            # print(imgfile)
-            # return imgfile
-
-            # file_raw = self.request.files["file"][0]["body"]
-            # print(type(file_raw))
-            #
-            # usr_home = os.path.expanduser('~')
-            # file_name = "./src/tmp/m_%s.jpg" % file_name
-            # print(file_name)
-
-            # fin = open(file_name, "w")
-            # print("success to open file")
-            # fin.write(file_raw)
-            # fin.close()
-
-            # file_name = "%s" % uuid.uuid1()
-            # file_raw = self.request.files["file"][0]["body"]
-            # usr_home = os.path.expanduser('~')
-            # file_name = usr_home + "./img/m_%s.jpg" % file_name
-            # fin = open(file_name, "w")
-            # fin.write(file_raw)
-            # fin.close()
 
             # json_result = {"count": 2, "upperLeft_X": "861", "upperLeft_Y": "538", "lowerRight_X": "1004", "lowerRight_Y": "658"}
             # self.render("user.html", username=json_result, email=file_name, website=file_raw, language=usr_home)
